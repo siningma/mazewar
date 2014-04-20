aa37713381f70ea83b43cb529554ee28e73c53ff
siningma <siningma@gmail.com>
2014-04-20 12:49:53 -0700

    new commit

--- toplevel.cpp
+++ toplevel.cpp
@@ -817,7 +817,7 @@ void printOtherRatsInfo() {
 		printf("Rat ratPosX: %u, ratPosY: %u, ratDir: %u, score: %d\n", it->second.rat.x.value(), it->second.rat.y.value(), it->second.rat.dir.value(), it->second.score);
 		printf("Missile exist: %u, missilePosX: %u, missilePosY: %u, missileSeqNum: %u\n", it->second.missile.exist, it->second.missile.x.value(), it->second.missile.y.value(), it->second.missile.seqNum);	
 	
-		// draw other rats score to the screen
+		// draw other rats score to the screen when score changes
 		if (strlen(it->second.ratName) > 0 && it->second.lastScore != it->second.score) {
 			UpdateScoreCard(MY_RAT_INDEX + i, it->second.ratName, it->second.score);
 			it->second.lastScore == it->second.score;
