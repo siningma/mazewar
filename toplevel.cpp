6b3d3aad37936d9900e1bb038358a9b868d79af7
siningma <siningma@gmail.com>
2014-04-20 17:48:34 -0700

    show missiles 4

--- toplevel.cpp
+++ toplevel.cpp
@@ -1004,6 +1004,7 @@ void manageMissiles()
 			// show missile if not hit the wall
 			showMissile(MY_MISSILE_X_LOC, MY_MISSILE_Y_LOC, 0, prevMissileXLoc, prevMissileYLoc, true);
 			updateView = TRUE;
+			DoViewUpdate();
 		}
 		lastMissilePosUpdateTime = getCurrentTime();
 	} 
